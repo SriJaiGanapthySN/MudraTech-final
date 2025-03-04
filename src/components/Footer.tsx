4aa1ac01b2a964038fb444985429ff379bb6c450
Sri Jai Ganapathy S N <jainthpadmasini80@gmail.com>
2025-03-04 22:39:28 +0530

    developer contact added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Mail, Phone, MapPin } from 'lucide-react';
+import { Mail, Phone, MapPin, Factory } from 'lucide-react';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-1 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h3 className="text-xl font-bold mb-4">Contact Us</h3>
             <div className="space-y-3">
@@ -23,19 +23,24 @@ const Footer = () => {
               </div>
             </div>
           </div>
-
-          {/* <div>
-            <h3 className="text-xl font-bold mb-4">Quick Links</h3>
-            <ul className="space-y-2">
-              <li><a href="/products" className="hover:text-blue-400 transition-colors">Products</a></li>
-              <li><a href="/expertise" className="hover:text-blue-400 transition-colors">Expertise</a></li>
-              <li><a href="/experience" className="hover:text-blue-400 transition-colors">Experience</a></li>
-            </ul>
-          </div> */}
-
-         
+          <div>
+            <h3 className="text-xl font-bold mb-4">Developed By</h3>
+            <div className="space-y-3">
+              <div className="flex items-center space-x-3">
+              <Factory className="h-5 w-5 text-blue-400" />
+                <span>Pyroguard Technologies</span>
+              </div>
+              <div className="flex items-center space-x-3">
+                <Mail className="h-5 w-5 text-blue-400" />
+                <a href="mailto:hello@pyroguard.co.in<"><span>hello@pyroguard.co.in</span></a>
+              </div>
+              <div className="flex items-center space-x-3">
+                <Phone className="h-5 w-5 text-blue-400" />
+                <a href="tel:+918637450865" target='blank'><span>+91  9894474945</span></a>
+              </div>
+            </div>
+          </div>
         </div>
-
         <div className="mt-8 pt-8 border-t border-gray-800 text-center">
           <p>&copy; {new Date().getFullYear()} Mudra Technologies. All rights reserved.</p>
         </div>
